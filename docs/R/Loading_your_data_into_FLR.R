7e6c3534baf3ce2d216b0cbd15c04c9d13324cd0
Iago Mosqueira <iago@mosqueira.eu>
2017-08-16 13:40:58 +0200

    Rerunning with corrections

--- docs/R/Loading_your_data_into_FLR.R
+++ docs/R/Loading_your_data_into_FLR.R
@@ -4,10 +4,10 @@ source("R/ini.R")
 
 ## ---- eval=FALSE---------------------------------------------------------
 ## install.packages(c("ggplot2"))
-## install.packages(c("FLcore", "ggplotFL"), repos="http://flr-project.org/R")
+## install.packages(c("FLCore","ggplotFL"), repos="http://flr-project.org/R")
 
 ## ---- pkgs---------------------------------------------------------------
-# This chunk loads all necessary packages, trims pkg messages
+# Load all necessary packages, trim pkg messages
 library(FLFleet)
 library(ggplotFL)
 
@@ -16,20 +16,18 @@ dir <- tempdir()
 download.file("http://flr-project.org/doc/src/loading_data.zip", file.path(dir, "loading_data.zip"))
 unzip(file.path(dir, "loading_data.zip"), exdir=dir)
 
-## ------------------------------------------------------------------------
+## ---- readcsv------------------------------------------------------------
 catch.n <- read.csv(file.path(dir,"catch_numbers.csv"), row=1)
-
-## ------------------------------------------------------------------------
+# We have read in the data as a data.frame
 class(catch.n)
 
-## ------------------------------------------------------------------------
+## ---- reshape------------------------------------------------------------
 catch.n.matrix <- as.matrix(catch.n)
 catch.n.matrix[,1:8]
 
-## ------------------------------------------------------------------------
+## ---- flquant------------------------------------------------------------
 catch.n.flq <- FLQuant(catch.n.matrix, dimnames=list(age=1:7, year = 1957:2011))
 catch.n.flq[,1:7]
-
 
 ## ---- readVPA------------------------------------------------------------
 # Read from a VPA text file
@@ -38,6 +36,7 @@ class(catch.n)
 
 ## ---- readFLStock--------------------------------------------------------
 # Read a collection of VPA files, pointing to the Index file:
+# DELETE: her <- readFLStock('http://flr-project.org/doc/src/her-irlw/index.txt')
 her <- readFLStock(file.path(dir,'her-irlw','index.txt'))
 class(her)
 
@@ -45,86 +44,71 @@ class(her)
 summary(her)
 
 ## ---- AddMissingAssessmentData-------------------------------------------
-her@stock.n <- readVPAFile(file.path(dir, "her-irlw", "n.txt"))
-print(her@stock.n[,ac(2007:2011)]) # only print 2007:2011
-
-her@harvest <- readVPAFile(file.path(dir,"her-irlw", "f.txt"))
-
+stock.n(her) <- readVPAFile(file.path(dir, "her-irlw", "n.txt"))
+print(stock.n(her)[,ac(2007:2011)]) # only print 2007:2011
+harvest(her) <- readVPAFile(file.path(dir,"her-irlw", "f.txt"))
 
 ## ---- CheckConsistency---------------------------------------------------
 # The sum of products (SOP)
-apply(her@landings.n * her@landings.wt, 2, sum)[,ac(2007:2011)]
+apply(landings.n(her)*landings.wt(her), 2, sum)[,ac(2007:2011)]
 # and the value read in from the VPA file
-her@landings[,ac(2007:2011)]
-
+landings(her)[,ac(2007:2011)]
 ## They are not the same!!  We correct the landings to be the same as the SOP - there is a handy function for this purpose
-her@landings <- computeLandings(her)
-
+landings(her) <- computeLandings(her)
 # In addition, there is no discard information
-her@discards.wt[,ac(2005:2011)]
-her@discards.n[,ac(2005:2011)]
+discards.wt(her)[,ac(2005:2011)]
+discards.n(her)[,ac(2005:2011)]
 # Set up the discards and catches
-her@discards.wt   <- her@landings.wt
-her@discards.n[]  <- 0
-her@discards      <- computeDiscards(her)
-her@catch         <- her@landings
-her@catch.wt      <- her@landings.wt
-her@catch.n       <- her@landings.n
-
+discards.wt(her)  <- landings.wt(her)
+discards.n(her)[] <- 0
+discards(her)     <- computeDiscards(her)
+catch(her)        <- landings(her)
+catch.wt(her)     <- landings.wt(her)
+catch.n(her)       <- landings.n(her)
 
 ## ---- Descriptions-------------------------------------------------------
 summary(her)
-
 #name and descriptions
-her@name # ok
-her@desc # ok
-
+name(her) # ok
+desc(her) # ok
 # Set the Fbar range for the stock 
-her@range[c('minfbar','maxfbar')] # ok, but can be filled with  <- c(min,max)
-
+range(her)[c('minfbar','maxfbar')] # ok, but can be filled with  <- c(min,max)
 # set the plus group
-her@range['plusgroup']  <- 7 # final year is a plusgroup
-
+range(her)['plusgroup']  <- 7 # final year is a plusgroup
 ## Units
-units(her@catch)    <- units(her@discards)    <- units(her@landings)    <- units(her@stock)    <- 'tonnes'
-units(her@catch.n)  <- units(her@discards.n)  <- units(her@landings.n)  <- units(her@stock.n)  <- '1000'
-units(her@catch.wt) <- units(her@discards.wt) <- units(her@landings.wt) <- units(her@stock.wt) <- 'kg'
-units(her@harvest) <- 'f'
+units(catch(her)) <- units(discards(her)) <- units(landings(her)) <- units(stock(her)) <- 'tonnes'
+units(catch.n(her)) <- units(discards.n(her)) <- units(landings.n(her)) <- units(stock.n(her)) <- '1000'
+units(catch.wt(her)) <- units(discards.wt(her)) <- units(landings.wt(her)) <- units(stock.wt(her)) <- 'kg'
+units(harvest(her)) <- 'f'
 
-
-## ---- Plot---------------------------------------------------------------
+## ---- Descriptions2------------------------------------------------------
 summary(her)
 plot(her) + theme_bw() # using the simple black and white theme
 
-## ------------------------------------------------------------------------
+## ---- readFLIndices------------------------------------------------------
 indices <- readFLIndices(file.path(dir, "ple4_ISIS.txt"))
 
-## ------------------------------------------------------------------------
+## ---- readFLIndices2-----------------------------------------------------
 indices <- read.table(file.path(dir, "ple4Index1.txt"))
-
-## ------------------------------------------------------------------------
+# transform into FLQuant
 indices <- FLQuant(as.matrix(indices), dimnames=list(age=1:8, year = 1985:2008))
-
-## ------------------------------------------------------------------------
+# and into FLIndex
 indices <- FLIndex(index = indices)
-
-## ------------------------------------------------------------------------
+# and then into FLIndices
 indices <- FLIndices(indices)
-
 plot(indices[[1]])
 
+## ---- rangeslot----------------------------------------------------------
+range(indices[[1]])[c('startf', 'endf')] <- c(0.66,0.75)
 
-## ------------------------------------------------------------------------
-indices[[1]]@range[c('startf', 'endf')] <- c(0.66,0.75)
-
-## ---- level table, echo = F----------------------------------------------
+## ---- leveltable, echo = F-----------------------------------------------
 kable(data.frame(Level = c(1,2,3),
 		 Class = c('FLFleet','FLMetier(s)','FLCatch(es)'),
 		 Contains = c('variables relating to vessel level activity',
-			     'variables relating to fishing level activity',
-			     'variables relating to stock catches')))
+			            'variables relating to fishing level activity',
+		              'variables relating to stock catches')))
 
-## ---- FLFleet slots------------------------------------------------------
+## ---- FLFleetslots-------------------------------------------------------
 # FLFleet level
 summary(FLFleet())
 # FLMetier level
@@ -134,18 +118,18 @@ summary(FLCatch())
 
 ## ---- Fleetdata, echo = F------------------------------------------------
 kable(data.frame(Fleet = c('Fleet1', 'Fleet2'),
-		 Metier = c('Metier1', 'Metier1'),
-		 Stock = c('Stock1', 'Stock2'),
-		 type = c('landings.n', 'landings.wt'),
-		 age = c(1,1),
-		 year = c(2011,2011),
-		 unit = c(1,1),
-		 season = c('all', 'all'),
-		 area = c('unique', 'unique'),
-		 iter = c(1,1),
-		 data = c(254,0.3)))
+		             Metier = c('Metier1', 'Metier1'),
+		             Stock = c('Stock1', 'Stock2'),
+	               type = c('landings.n', 'landings.wt'),
+		             age = c(1,1),
+		             year = c(2011,2011),
+		             unit = c(1,1),
+		             season = c('all', 'all'),
+		             area = c('unique', 'unique'),
+		             iter = c(1,1),
+		             data = c(254,0.3)))
 
-## ---- Generating FLFleets, eval = F--------------------------------------
+## ---- GeneratingFLFleets, eval = F---------------------------------------
 ## # Example of generating fleets
 ## fl.nam <- unique(data$Fleet) # each of the fleets
 ## 
@@ -218,5 +202,4 @@ kable(data.frame(Fleet = c('Fleet1', 'Fleet2'),
 ## 		 }))
 ## 
 ## names(fleets) <- fl.nam
-## 
 
