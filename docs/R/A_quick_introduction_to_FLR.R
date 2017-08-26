f73e46582f8e5f4925f9b5bde3a90a72f2545a46
iagomosqueira <iago@mosqueira.eu>
2017-08-26 21:11:16 +0200

    Updated FLash

--- docs/R/A_quick_introduction_to_FLR.R
+++ docs/R/A_quick_introduction_to_FLR.R
@@ -3,12 +3,54 @@ library(knitr)
 source("R/ini.R")
 
 ## ---- eval=FALSE---------------------------------------------------------
-## install.packages(c("latticeExtra", "gridExtra", "ggplot2"))
-## install.packages(c("FLCore", "ggplotFL"), repos="http://flr-project.org/R")
+## install.packages(c("latticeExtra", "gridExtra", "ggplot2", "triangle", "copula", "coda", "mgcv"))
+## install.packages(c("FLCore", "ggplotFL", "FLa4a"), repos="http://flr-project.org/R")
 
 ## ---- flcore-------------------------------------------------------------
 library(FLCore)
 library(ggplotFL)
+
+## ---- flquant------------------------------------------------------------
+FLQuant(1:10)
+
+## ---- flquantage---------------------------------------------------------
+flq <- FLQuant(rlnorm(60), dimnames=list(age=1:4, year=2012:2017), units="t")
+
+flq
+
+## ---- flquantinspect-----------------------------------------------------
+# A summary of structure and data
+summary(flq)
+
+# dimnames
+dimnames(flq)
+
+# dims
+dim(flq)
+
+# units
+units(flq)
+
+## ---- flquantmodify------------------------------------------------------
+# Extract first year
+flq[, 1]
+
+# Extract year 2013
+flq[, "2013"]
+
+# Set catches on age 1 to zero
+flq[1,] <- 0
+flq
+
+## ---- flquantarith-------------------------------------------------------
+# Product with scalar
+flq * 10
+
+# Addition with another FLQuant
+flq + (flq * 0.20)
+
+# Sum along years
+yearSums(flq)
 
 ## ---- getfiles, message=FALSE--------------------------------------------
 dir <- tempdir()
@@ -51,6 +93,9 @@ discards(ple4) <- computeDiscards(ple4)
 ## ---- ple4catch----------------------------------------------------------
 catch(ple4) <- computeCatch(ple4, slot="all")
 
+## ---- ple4stockwt--------------------------------------------------------
+stock.wt(ple4) <- catch.wt(ple4)
+
 ## ---- ple4range----------------------------------------------------------
 range(ple4, c("minfbar", "maxfbar")) <- c(2, 6)
 
@@ -69,4 +114,35 @@ plot(ple4.index)
 
 ## ---- ple4indexrange-----------------------------------------------------
 range(ple4.index)[c("startf", "endf")]
+
+## ---- fla4apkg-----------------------------------------------------------
+library(FLa4a)
+
+## ---- sca----------------------------------------------------------------
+fit <- sca(ple4, FLIndices(BTS=ple4.index))
+
+## ---- summarya4afit------------------------------------------------------
+summary(fit)
+
+## ---- ple4fit------------------------------------------------------------
+stk <- ple4 + fit
+plot(stk)
+
+## ---- flsrcreate---------------------------------------------------------
+plsr <- as.FLSR(stk)
+
+## ---- flsrsummary--------------------------------------------------------
+summary(plsr)
+
+## ---- flsrmodel----------------------------------------------------------
+model(plsr) <- ricker()
+
+## ---- flsrfit, results="hide"--------------------------------------------
+plsr <- fmle(plsr)
+
+## ---- flsrprofile--------------------------------------------------------
+profile(plsr)
+
+## ---- flsrpredict--------------------------------------------------------
+predict(plsr, ssb=FLQuant(rnorm(10, 25e4, sd(ssb(plsr))), dimnames=list(age=1, year=2008:2017)))
 
